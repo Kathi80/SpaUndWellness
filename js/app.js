fc3a5a50ebbb28e1f4593dab8b34d66bd43e73f8
Kathi80 <kachasas@gmx.de>
2020-07-20 13:08:47 +0200

    nested menus

--- js/app.js
+++ js/app.js
@@ -2,17 +2,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     
     // nested menu
-    const nestedMenu = document.querySelector(".nested-menu");
-    const menuTriggerEl = document.querySelector(".menu-trigger-el");
+    const nestedMenu = document.querySelectorAll(".nested-menu");
+    const menuTriggerEl = document.querySelectorAll(".menu-trigger-el");
 
-    
-    menuTriggerEl.addEventListener("mouseover", function() {
-       nestedMenu.style.display = "block";
-    });
-    
-    nestedMenu.addEventListener("mouseout", function() {
-        this.style.display = "none";
+    menuTriggerEl.forEach((element,key) => {
+        element.addEventListener("mouseover", function() {
+            nestedMenu[key].style.display = "block";
+         });
     });
 
+    nestedMenu.forEach(element => {
+        element.addEventListener("mouseout", function() {
+            this.style.display = "none";
+        });
+    });
+    
+  
+    
 
+    
+    
 });
